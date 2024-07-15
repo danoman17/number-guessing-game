bc1e2eab7fd7e5f6ab234f2ff12d104916b5cdc7
Daniel Flores Rodríguez <daniflores374@gmail.com>
2024-07-15 20:16:37 +0000

    feat: welcome messages display

--- number_guess.sh
+++ number_guess.sh
@@ -4,5 +4,14 @@
 #generates a random number between 1 - 1000
 RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
 
+echo -e "\n~~~~~~ Welcome to the Guessing Number Game ~~~~~~"
+
+echo -e "\nEnter your username:"
+
+read USER_NAME
+
+echo "Your name is: $USER_NAME"
+
+
 #display the number generated
-echo $RANDOM_NUMBER
+echo -e "Number generated: $RANDOM_NUMBER"
