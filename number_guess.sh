7babd0d1b9104184e8d88cd19bee41a8a4c0497b
Daniel Flores Rodríguez <daniflores374@gmail.com>
2024-07-15 22:42:48 +0000

    feat: user name input message display

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#PSQL variable
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+
+read_number_input(){
+  echo -e "\nGuess the secret number between 1 and 1000:"
+
+  #TODO: validate the input to be a integer
+  #TODO: set guides for user's attemps
+  #TODO: create a counter for attemps
+  read NUMBER_GUESS
+
+  echo -e "\nThe guess is: $NUMBER_GUESS"
+
+
+}
+
 
 #generates a random number between 1 - 1000
 RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
@@ -10,8 +27,17 @@ echo -e "\nEnter your username:"
 
 read USER_NAME
 
-echo "Your name is: $USER_NAME"
+DUMP_NAME="a"
 
+if [[ -z $DUMP_NAME ]]
+then
+  echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
 
-#display the number generated
-echo -e "Number generated: $RANDOM_NUMBER"
+else
+  #TODO: get games_played and best_game from DB.
+
+  echo -e "\nWelcome back, $USER_NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+
+  read_number_input
+
+fi
